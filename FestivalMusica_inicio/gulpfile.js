5f2a845fa5c535c141c3908b109ad1e703ca3923
Diego C <diegocs2410@gmail.com>
2021-01-20 17:17:21 -0500

    GaleriaJS

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -3,11 +3,14 @@ const sass = require('gulp-sass');
 const imageMin = require('gulp-imagemin')
 const notify = require('gulp-notify')
 const imgWebp = require('gulp-webp')
+const concat = require('gulp-concat')
+
 // Funcion que compila SASS
 
 const paths = {
     imagenes: 'src/img/**/*',
-    scss: 'src/scss/**/*.scss'
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
 }
 function css() {
     return src(paths.scss)
@@ -15,6 +18,11 @@ function css() {
             outputStyle: 'expanded'
         }))
         .pipe(dest('./build/css'))
+}
+function javascript() {
+    return src(paths.js)
+        .pipe(concat('bundle.js'))
+    .pipe(dest('build/js'))
 }
 function minificarCss() {
     return src(paths.scss)
@@ -37,9 +45,10 @@ function versionWebp() {
 }
 function watchCss() {
     watch(paths.scss, css)/* *= La carpeta Actual - ** = Todos los archivos con esa extension */
+    watch(paths.js, javascript)
 }
 exports.minificarCss = minificarCss;
 exports.css = css;
 exports.imagenes = imagenes;
 exports.watchCss = watchCss;
-exports.default = series(css, imagenes, versionWebp, watchCss);
+exports.default = series(css, imagenes, javascript, versionWebp, watchCss);
