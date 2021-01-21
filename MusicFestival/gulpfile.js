38502202e26269f07462f6acb49900bddad77444
Diego C <diegocs2410@gmail.com>
2021-01-20 19:55:44 -0500

    1er Compilacion Archivo Scss a Css

--- MusicFestival/gulpfile.js
+++ MusicFestival/gulpfile.js
@@ -1,10 +1,11 @@
-const {series} = require('gulp')
+const { series, src, dest } = require('gulp')
 const sass = require('gulp-sass')
 
 // Funcion que compila SASS
-function css(done) {
-    console.log('Compilando SASS')
-    done()
+function css() {
+    return src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('./build/css'))
 }
 
 exports.css = css
