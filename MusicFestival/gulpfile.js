92d0a49fb522ddcf0220630e9b3b41a807e69a86
Diego C <diegocs2410@gmail.com>
2021-01-23 16:31:47 -0500

    lineup Creado

--- MusicFestival/gulpfile.js
+++ MusicFestival/gulpfile.js
@@ -2,17 +2,23 @@ const { series, src, dest, watch, parallel } = require('gulp')
 const sass = require('gulp-sass')
 const imageMin = require('gulp-imagemin')
 const notify = require('gulp-notify')
+const webp = require('gulp-webp')
+
+const paths = {
+    pImagenes: 'src/img/**/*',
+    pAppScss: 'src/scss/**/*.scss'
+}
 
 // Funcion que compila SASS
 function css() {
-    return src('src/scss/app.scss')
+    return src(paths.pAppScss)
         .pipe(sass({
             outputStyle: 'expanded'
         }))
         .pipe(dest('./build/css'))
 }
 function minificarCss() {
-    return src('src/scss/app.scss')
+    return src(paths.pAppScss)
         .pipe(sass({
             outputStyle: 'compressed'
         }))
@@ -20,18 +26,23 @@ function minificarCss() {
 }
 
 function imagenes() {
-    return src('src/img/**/*')
+    return src(paths.pImagenes)
         .pipe(imageMin())
         .pipe(dest('./build/img'))
     .pipe(notify({message: 'Imagen Minificada'}))
 }
+function versionWebp() {
+    return src(paths.pImagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+        .pipe(notify({ message: 'Version Webp' }))
+}
 
 
 function watchCss() {
-    watch('src/scss/**/*.scss', css)
+    watch(paths.pAppScss, css)
 }
 exports.css = css
 exports.imagenes = imagenes
 exports.watchCss = watchCss
-
-exports.defaults = series(css, imagenes, watchCss)
+exports.default = series(css, imagenes, versionWebp, watchCss)
